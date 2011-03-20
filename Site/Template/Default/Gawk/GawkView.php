fa03f75ba4fa017745b040efeac18b49afadcaa0
Elliot Coad <elliotcoad@gmail.com>
2011-03-20 17:02:53 +0000

    Implemented change wall select box

--- Site/Template/Default/Gawk/GawkView.php
+++ Site/Template/Default/Gawk/GawkView.php
@@ -6,25 +6,25 @@
 	<p>The main wall is a collection of the latest and highly rated Gawks.</p>
 </div>
 <div class="wall-select" style="float:right; padding: 5px;">
-	<form class="select-wall" method="get" action="">
+	<form class="select-wall" method="get" action="" onchange="window.location = $('select[name=SelectWall]').val();">
 		<fieldset>
 			<label>
 				<strong>Select Wall</strong><br />
-				<select name="WallSecureId">
-					<option value="s8df7s89d7sd9ssdf">Main</option>
+				<select name="SelectWall">
+					<option value="/">Main</option>
 					<optgroup label="Main">
-						<option value="s8df7s89d7sd98">Main</option>
-						<option value="s8dewers89d7sd98">Friends</option>
+						<option value="/">Main</option>
+						<option value="/friends">Friends</option>
 					</optgroup>
 					<optgroup label="My Walls">
-						<option value="s8df7sf3ef9d7sd98">John's Wedding</option>
-						<option value="s8df7s89dwerasd98">Australia 2011</option>
+						<option value="/johns-wedding">John's Wedding</option>
+						<option value="/australia-2011">Australia 2011</option>
 					</optgroup>
 					<optgroup label="Bookmarks">
-						<option value="s8df7s89d7dfgdfg8">Clock Ltd</option>
-						<option value="s8df7s89d7sd34frer8">Humhum Forum</option>
+						<option value="/clock-ltd">Clock Ltd</option>
+						<option value="/humhum-forum">Humhum Forum</option>
 					</optgroup>
-					<option value="create">Create your own wall&hellip;</option>
+					<option value="/wall/create">Create your own wall&hellip;</option>
 				</select><br />
 			</label>
 		</fieldset>
--- Site/Template/Default/Widget/Navigation.php
+++ Site/Template/Default/Widget/Navigation.php
@@ -1,7 +1,7 @@
 <ul id="navigation-widget">
-  <li><a class="new-gawk" href="#">New Gawk</a></li>
-  <li><a class="new-wall">New Wall</a></li>
-  <li><a class="yours">Yours</a></li>
+  <li><a class="new-gawk" href="#">Gawk</a></li>
+  <li><a class="new-wall">Wall</a></li>
+  <li><a class="yours">Profile</a></li>
 </ul>
 <a href="#"><img width="170" style="float:left;" src="http://nekodot.jp/appstore.gif" /></a>
 <hr />
