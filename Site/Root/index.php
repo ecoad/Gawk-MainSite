b91576dffcfbe2334a62f146bb4fc0bab1c21704
Elliot Coad <elliotcoad@gmail.com>
2011-03-22 00:43:41 +0000

    Updated routing to permit custom Wall URLs

--- Site/Root/index.php
+++ Site/Root/index.php
@@ -19,8 +19,11 @@ $layout->start("Main");
 		<div id="gawk-view" style="display: none;">
 <?php include "Site/Template/Default/Gawk/GawkView.php"; ?>
 		</div>
-		<div id="wall-view" style="display: none;">
-<?php include "Site/Template/Default/Gawk/WallView.php"; ?>
+		<div id="wall-select-view" style="display: none;">
+<?php include "Site/Template/Default/Gawk/Wall/WallSelectView.php"; ?>
+		</div>
+		<div id="wall-edit-view" style="display: none;">
+<?php include "Site/Template/Default/Gawk/Wall/WallEditView.php"; ?>
 		</div>
 		<div id="yours-view" style="display: none;">
 <?php include "Site/Template/Default/Gawk/YoursView.php"; ?>
@@ -37,12 +40,15 @@ $layout->start("JavaScript");
 	<script type="text/javascript" src="/resource/js/application/gawk/widgets/login-widget.js?v=@VERSION-NUMBER@"></script>
 	<script type="text/javascript" src="/resource/js/application/gawk/widgets/navigation-widget.js?v=@VERSION-NUMBER@"></script>
 	<script type="text/javascript" src="/resource/js/application/gawk/views/login-view.js?v=@VERSION-NUMBER@"></script>
-	<script type="text/javascript" src="/resource/js/application/gawk/views/wall-view.js?v=@VERSION-NUMBER@"></script>
+	<script type="text/javascript" src="/resource/js/application/gawk/views/wall-select-view.js?v=@VERSION-NUMBER@"></script>
+	<script type="text/javascript" src="/resource/js/application/gawk/views/wall-edit-view.js?v=@VERSION-NUMBER@"></script>
 	<script type="text/javascript" src="/resource/js/application/gawk/views/yours-view.js?v=@VERSION-NUMBER@"></script>
 	<script type="text/javascript" src="/resource/js/application/gawk/views/gawk-view.js?v=@VERSION-NUMBER@"></script>
 	<script type="text/javascript" src="/resource/js/application/gawk/member/member-control.js?v=@VERSION-NUMBER@"></script>
+	<script type="text/javascript" src="/resource/js/application/gawk/wall/wall-control.js?v=@VERSION-NUMBER@"></script>
 	<script type="text/javascript" src="/resource/js/application/gawk/gawk.js?v=@VERSION-NUMBER@"></script>
 	<script type="text/javascript" src="/resource/js/swfobject/swfobject.js?v=@VERSION-NUMBER@"></script>
+	<script type="text/javascript" src="/resource/js/jquery/jquery.placeholder.js"></script>
 	<script type="text/javascript">
 //<![CDATA[
 $(document).ready(function() {
