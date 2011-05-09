3bc96ce57799bfd9b67290893326fab6f9c38440
Elliot Coad <elliotcoad@gmail.com>
2011-05-09 11:01:40 +0100

    Added FOWD wall

---
+++ Support/Database/Delta/2011-05-09a-AddingWallForFowd.sql
@@ -0,0 +1,2 @@
+INSERT INTO "Wall"("SecureId", "Description", "Url", "MemberSecureId", "Name")
+    VALUES ('gawk', 'Clock\'s Gawk Booth @ The Future of Web Design 2011', 'fowd-2011', 'u-fowd', 'FOWD 2011');
