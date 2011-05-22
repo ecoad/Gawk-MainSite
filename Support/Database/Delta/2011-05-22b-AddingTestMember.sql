cd257e3c0080ff10406aa65d62434948af9a2c46
ecoad <elliotcoad@gmail.com>
2011-05-22 12:40:30 +0100

    Added test member

---
+++ Support/Database/Delta/2011-05-22b-AddingTestMember.sql
@@ -0,0 +1,6 @@
+INSERT INTO "Member"(
+            "EmailAddress", "Alias", "Password", "FirstName", "LastName", 
+            "ConfirmationId", "SecureId")
+    VALUES ('test@clock.co.uk', 'test-man', 
+	'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4', 'Test', 'Man', 'testconf', 
+            'u-testman');
