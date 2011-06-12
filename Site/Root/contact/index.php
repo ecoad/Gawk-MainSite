91604e217135ae94488522137568ae559760d584
Elliot Coad <elliotcoad@gmail.com>
2011-06-12 16:52:34 +0100

    Contact form

--- Site/Root/contact/index.php
+++ Site/Root/contact/index.php
@@ -16,25 +16,37 @@ $layout->set("Title", "contact / " . $application->registry->get("Title"));
 $layout->set("Name", $application->registry->get("Title"));
 $layout->set("Section", "contact");
 $layout->start("Style");
+?>
+<link media="all" href="/resource/css/basic-form.css?v=@VERSION-NUMBER@" type="text/css" rel="stylesheet"/>
+<?php
 $layout->start("Main");
 // The main page content goes here.
 ?>
-<div class="breadcrumb">
-	<a href="/">home</a> / <a title="contact us" href="/contact">contact</a>
+<div id="title-area">
+	<div class="breadcrumb">
+		<a href="/">home</a> / <a title="contact us" href="/contact">contact</a>
+	</div>
 </div>
 <div id="contact-view">
-	<h1>contact</h1>
-	<form method="post" action="">
-		<label>
-			<strong class="required">email<span>*</span></strong><br />
-			<input name="<?php echo $postEmailAddress; ?>" type="email" class="textbox medium"/>
-		</label><br />
-		<label>
-			<strong class="required">message<span>*</span></strong><br />
-			<textarea class="textbox wide tall" rows="10" cols="40" name="<?php echo $postMessage; ?>"></textarea>
-		</label>
-		<input type="submit" name="Submit" value="<?php echo $postAction; ?>">
-	</form>
+	<div class="view-container">
+		<h1 class="page-title">contact us</h1>
+		<p class="white-normal">Just enter your email and your message and we’ll get back to you as soon as we can.</p>
+		<form method="post" action="" class="basic-form">
+			<div class="contact-graphic"></div>
+			<label>
+				<strong class="required">email<span>*</span></strong>
+				<input name="<?php echo $postEmailAddress; ?>" type="email" class="textbox medium"/>
+			</label>
+			<label>
+				<strong class="required">message<span>*</span></strong>
+				<textarea class="textbox wide tall" rows="10" cols="40" name="<?php echo $postMessage; ?>"></textarea>
+			</label>
+			<a href="#" onclick="$(this).parents('form').submit();" class="submit" title="submit the form">
+				<span><?php echo $postAction; ?></span>
+			</a>
+			<input type="hidden" name="Submit" value="<?php echo $postAction; ?>"/>
+		</form>
+	</div>
 </div>
 <?php
 $layout->start("JavaScript");
@@ -44,7 +56,6 @@ $layout->start("JavaScript");
 $(document).ready(function() {
 	var gawk = new Gawk({
 		initView: "ContactView",
-		member: "<?php echo addslashes(json_encode($member)); ?>",
 		apiLocation: "<?php echo $application->registry->get("Site/Address"); ?>/api/",
 		fbAppId: "<?php echo $facebook->getAppId(); ?>",
 		fbSession: <?php echo json_encode($facebook->getSession()); ?>
